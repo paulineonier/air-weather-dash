d85df88bd848e85003b8517bac59c30a7153c8e2
paulineSWE <pauline.onier@holbertonschool.com>
2026-08-20 11:47:11 +0200

    fix: correction des chemins de build pour GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/air-weather-dash/', // Nécessaire pour GitHub Pages
+  // Le slash final est crucial
+  base: './', 
 });
